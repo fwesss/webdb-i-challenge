37bc1431fd3030cc059f06f0a8153b881a47b2db
Westley Feller <feller.westley@gmail.com>
2020-01-30 12:01:17 -0800

    Logging with morgan

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express, { json } from 'express'
+import morgan from 'morgan'
 import { serve, setup } from 'swagger-ui-express'
 
 import swaggerDocument from './openapi.json'
@@ -6,7 +7,9 @@ import swaggerDocument from './openapi.json'
 import db from './data/dbConfig'
 
 const server = express()
+
 server.use('/api-docs', serve, setup(swaggerDocument))
+server.use(morgan('dev'))
 server.use(json())
 
 server.get('/', (_req, res) => res.send(`<h1>Node API Challenge</h1>`))
