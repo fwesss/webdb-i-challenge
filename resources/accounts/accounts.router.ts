7ca90e2f21c0d594537f17c367568d8c09315083
Westley Feller <feller.westley@gmail.com>
2020-01-30 18:02:56 -0800

    400 validation

--- resources/accounts/accounts.router.ts
+++ resources/accounts/accounts.router.ts
@@ -1,17 +1,73 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import Validation from 'folktale/validation'
+import { error as logError } from 'console'
+import { findById } from './accounts.model'
+import { validator, didItValidate } from '../../utils/validator'
 import controllers from './accounts.controllers'
 
+const { Success } = Validation
+
 const router = express.Router()
+
+const hasBody = (req: Request): boolean => !!req.body
+const hasName = (req: Request): boolean => !!req.body.name
+const hasBudget = (req: Request): boolean => !!req.body.budget
+
+const bodyValidator = validator('Missing account data', hasBody)
+const nameValidator = validator('Missing name', hasName)
+const budgetValidator = validator('Missing budget', hasBudget)
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const accountValidationResult = (req: Request): any =>
+  Success()
+    .concat(bodyValidator(req))
+    .concat(nameValidator(req))
+    .concat(budgetValidator(req))
+
+const validateAccount = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const didProjectValidate = didItValidate(accountValidationResult(req))
+  if (!didProjectValidate) {
+    res.status(400).json({ errors: accountValidationResult(req).value })
+  } else {
+    next()
+  }
+}
+
+const validateAccountId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const project = await findById(req.params.id)
+    if (project) {
+      next()
+    } else {
+      res.status(400).json({ error: 'Invalid account ID' })
+    }
+  } catch (error) {
+    logError(error)
+    res
+      .status(500)
+      .json({ error: 'Account information could not be retrieved' })
+  }
+}
+
+router.use('/:id', validateAccountId)
 
 router
   .route('/')
   .get(controllers.getMany)
-  .post(controllers.createOne)
+  .post(validateAccount, controllers.createOne)
 
 router
   .route('/:id')
   .get(controllers.getOne)
-  .put(controllers.updateOne)
+  .put(validateAccount, controllers.updateOne)
   .delete(controllers.removeOne)
 
 export default router
