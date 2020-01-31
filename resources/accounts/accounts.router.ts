d3ba8536dc7e96ca20d2001b16c61139af89bbe1
Westley Feller <feller.westley@gmail.com>
2020-01-30 21:10:23 -0800

    Refactored all 500s to be handled by dbErrorHandler

--- resources/accounts/accounts.router.ts
+++ resources/accounts/accounts.router.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express'
 import Validation from 'folktale/validation'
-import { error as logError } from 'console'
-import { findById, findByName } from './accounts.model'
+import { findById } from './accounts.model'
 import { validator, didItValidate } from '../../utils/validator'
 import controllers from './accounts.controllers'
+import dbErrorHandler from '../../utils/dbErrorHandler'
 
 const { Success } = Validation
 
@@ -47,13 +47,10 @@ const validateAccountId = async (
     if (project) {
       next()
     } else {
-      res.status(400).json({ error: 'Invalid account ID' })
+      res.status(404).json({ error: 'Invalid account ID' })
     }
   } catch (error) {
-    logError(error)
-    res
-      .status(500)
-      .json({ error: 'Account information could not be retrieved' })
+    dbErrorHandler(error, res)
   }
 }
 
